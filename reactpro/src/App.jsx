3950cc24639816d661d00da231e98cdfe5a567f3
Pratik Patel <pratspatel17791@gmail.com>
2024-02-03 15:31:17 -0600

    first component

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,11 +1,12 @@
+import Hello from "./components/Hello";
 
 function App() {
-
   return (
     <div className="App">
-      <h1> Hello World!! its my Vite + React APP</h1>
+      <h1> Welcome to my first Vite + React APP </h1>
+      <Hello/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ reactpro/src/components/Hello.jsx
@@ -0,0 +1,7 @@
+const Hello = () => {
+    return ( 
+        <h1>Hello From component!!</h1>
+     );
+}
+ 
+export default Hello;
