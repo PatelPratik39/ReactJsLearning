da41b2efcaf7e4c5752a553657b8011eb4f98749
Pratik Patel <pratspatel17791@gmail.com>
2024-02-03 23:58:23 -0600

    dd

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,11 +1,24 @@
 import Hello from "./components/Hello";
 
 function App() {
+  // const seatNumbers = 39;
+  const person = {
+    name: "Rob",
+    message: "Hi There",
+    emoji: "😶‍🌫️",
+    seatNumbers: 123
+  };
   return (
     <div className="App">
       <h1> Welcome to my first Vite + React APP </h1>
       {/* <Hello name="Ronald" message="Hi There!!" /> */}
-      <Hello name="Harry" message="Are you there?? !!" emoji='🙁'/>
+      {/* <Hello
+        name="Harry"
+        message="Are you there?? !!"
+        emoji="🙁"
+        seatNum={seatNumbers}
+      /> */}
+      <Hello person={person} />
     </div>
   );
 }
--- reactpro/src/components/Hello.jsx
+++ reactpro/src/components/Hello.jsx
@@ -1,13 +1,14 @@
-const Hello = ({name, message , emoji}) => {
-    // console.log(props);
-    // const[name, setName] = props;   we dont need this declaration of props because I have declared props in function
-    return (
-      <div>
-        <h1>Hello From component!!</h1>
-        {/* <h2> {props.message} {props.name}</h2> */}
-        {message} {name} {emoji}
-      </div>
-    );
-}
- 
+// const Hello = (props) => {
+const Hello = ({ person }) => {
+      // console.log(props);
+      // const[name, setName] = props;   // ******** I dont need this declaration of props because I have declared props in function
+      return (
+        <div>
+          <h1>Hello From component!!</h1>
+          {/* <h2> {props.message} {props.name}</h2> */}
+          {person.message} {person.name} {person.emoji} {person.seatNumbers}
+        </div>
+      );
+    };
+
 export default Hello;
