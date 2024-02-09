fb06229aa1ca626641883b5fbdd284c1278c7dd2
Pratik Patel <pratspatel17791@gmail.com>
2024-02-09 13:59:04 -0600

    Item is completed or not

--- Todo/todo/src/Components/Form.jsx
+++ Todo/todo/src/Components/Form.jsx
@@ -3,13 +3,14 @@ import styles from './form.module.css';
 
 
 const Form = ({todos, setTodos}) => {
-  const [todo, setTodo] = useState("");
-  // const [todos, setTodos] = useState([]);
-  
+
+  const [todo, setTodo] = useState({name:'', done: false});
+
     function handleSubmit(e) {
       e.preventDefault();
       setTodos([...todos, todo]);
-      setTodo([]);
+      // setTodo([]);
+      setTodo({ name: "", done: false });
     }
     return (
       <form className={styles.todoform} onSubmit={handleSubmit}>
@@ -17,9 +18,10 @@ const Form = ({todos, setTodos}) => {
           <input
             className={styles.modernInput}
             type="text"
-            value={todo}
+            value={todo.name}
             placeholder="Enter Todo Item.."
-            onChange={(e) => setTodo(e.target.value)}
+            // onChange={(e) => setTodo(e.target.value)}
+            onChange={(e) => setTodo({name: e.target.value, done: false})}
           />
           <button className={styles.modernButton} type="submit">
             Add
--- Todo/todo/src/Components/TodoItem.jsx
+++ Todo/todo/src/Components/TodoItem.jsx
@@ -1,14 +1,27 @@
-import styles from './TodoItem.module.css'
+import styles from "./TodoItem.module.css";
 
 const TodoItem = ({ item, todos, setTodos }) => {
   function handleDelete(item) {
     console.log("Delete Clicked for an item ", item);
-    setTodos(todos.filter((todo) => todo !== item ));
+    setTodos(todos.filter((todo) => todo !== item));
   }
+
+  function handleClick(name) {
+    // console.log('Item ckicked', name);
+    const newArray = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newArray);
+  }
+  // this line make itemlist checked that completed ot not
+  const className = item.done ? styles.completed : " ";
+  
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item}{" "}
+        <span className={className} onClick={() => handleClick(item.name)}>
+          {item.name}{" "}
+        </span>
         <span>
           <button
             onClick={() => handleDelete(item)}
@@ -24,5 +37,5 @@ const TodoItem = ({ item, todos, setTodos }) => {
     </div>
   );
 };
- 
+
 export default TodoItem;
