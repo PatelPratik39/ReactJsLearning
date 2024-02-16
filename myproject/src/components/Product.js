8c4276f7007d5f4f7486a35c487fd568a036130a
PatelPratik39 <pratspatel17791@gmail.com>
2024-02-15 23:52:47 -0600

    cd

--- myproject/src/components/Product.js
+++ myproject/src/components/Product.js
@@ -1,5 +1,5 @@
 const Product = () => {
-    const products = ['Laptop','desktop', 'phone','Modem']
+    const products = ['Laptop','desktop', 'phone','Modem', 'Ipad']
     const productList = products.map((product) => <h3>{product}</h3>);
     return ( <div>
         {productList }
