c8a2bb65aeb37b0695c39f03a3bcc45df331249d
PatelPratik9 <pratspatel17791@gmail.com>
2024-02-16 00:18:01 -0600

    nhnhn

--- myproject/src/components/Product.js
+++ myproject/src/components/Product.js
@@ -1,6 +1,14 @@
 const Product = () => {
-    const products = ['Laptop','desktop', 'phone','Modem', 'Ipad']
-    const productList = products.map((product) => <h3>{product}</h3>);
+    // const products = ["Laptop", "desktop", "phone", "Modem", "Ipad", "Laptop"];
+    const products = [
+      { id: 1, name: "Laptop", price: 500 },
+      { id: 2, name: "Desktop", price: 4500 },
+      { id: 3, name: "Iphone", price: 1500 },
+      { id: 4, name: "Modem", price: 50 },
+      { id: 5, name: "Ipad", price: 200 },
+      { id: 6, name: "Laptop", price: 500 }
+    ];
+    const productList = products.map((product) => <h3 key={product.id}>{product.name} : $ {product.price}</h3>);
     return ( <div>
         {productList }
     </div> );
