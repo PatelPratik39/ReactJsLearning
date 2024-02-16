16d04d996e3823ca8e6e7b3ac68fa97069d29ee9
Pratik Patel <pratspatel17791@gmail.com>
2024-02-15 23:03:26 -0600

    frf

---
+++ myproject/src/components/ConditionalComponent copy.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const ConditionalComponent = () => {
+  const [display, setDisplay] = useState(true);
+
+  return display ? (
+    <div>
+      <h2>This is Conditional Component</h2>
+    </div>
+  ) : (
+    <div>
+      <h2>Nothing to see HERE!!</h2>
+      <p></p>
+    </div>
+  );
+};
+
+export default ConditionalComponent;
---
+++ myproject/src/components/Counter copy.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+class Counter extends Component {
+  // AS per as OOPs concept, we all know that whne we call  a object in class, we have to have constructor()
+  // in order to use the object
+
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+  }
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+    // alert('Ohh, why did You click me????')
+  }
+  decrement() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+    // alert('Ohh, why did You click me????')
+  }
+  render() {
+    return (
+      <div>
+        <h3> Count Value is : {this.state.counter}</h3>
+        <button onClick={() => this.increment()}> Increment</button>
+        <br />
+        <button onClick={() => this.decrement()}> Decrement</button>
+      </div>
+    );
+  }
+}
+export default Counter;
