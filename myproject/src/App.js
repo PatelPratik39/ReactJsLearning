ef5d6f8ff631b92f5612577a61a0795c9d11aff4
PatelPratik9 <pratspatel17791@gmail.com>
2024-02-21 16:12:07 -0600

    style

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,39 +1,11 @@
 import "./App.css";
-// import Form from "./components/Form";
-import InlineComponent from "./components/InlineComponent";
-// import Todo from "./components/Todo";
-// import ConditionalComponent from "./components/ConditionalComponent";
-// import Product from "./components/Product";
-// import ConditionalComponent from "./components/ConditionalComponent";
-// import FunctionEvent from "./components/FunctionEvent";
-// import Counter from "./components/Counter";
-// import FunctionalCounter from "./components/FunctionalCounter";
-// import Message from "./components/Message";
-// import Profile from "./components/Profile";
-// import Resume from "./components/Resume";
-// import ClassEvent from "./components/ClassEvent";
+import StylesheetComponent from "./components/stylesheetComponent";
 
 function App() {
   return (
     <div className="App">
       <h1> It's My ReactJS Project...🧑🏻‍💻</h1>
-      {/* <Profile name="Pratik" lastName="Patel"/> */}
-      {/* <Message messageContent = ' This is a message from Props .. '/> */}
-      {/* <Counter /> */}
-      {/* <FunctionalCounter/> */}
-      {/* <ConditionalComponent /> */}
-
-      {/* <Product/> */}
-      {/* <Form/> */}
-      {/* <Resume name = 'Mowglee'/> */}
-      {/* <FunctionEvent/> */}
-      {/* <ClassEvent /> */}
-    {/* <Todo/> */}
-    <InlineComponent/>
-      {/* <Resume name = 'Mowglee'/> */}
-      {/* <FunctionEvent/> */}
-      {/* <ClassEvent /> */}
-      {/* <Product/> */}
+      <StylesheetComponent />
     </div>
   );
 }
---
+++ myproject/src/components/stylesheetComponent.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const StylesheetComponent = () => {
+  return <div className="header">This is style sheet component</div>;
+};
+
+export default StylesheetComponent;
